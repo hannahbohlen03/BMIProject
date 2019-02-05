0ad9574d3f7a9cefe0b0e60c1a52032674eb0d49
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-02-05 07:24:01 -0500

    New Class + Declare

---
+++ src/com/LickingHeights/BMI.java
@@ -0,0 +1,14 @@
+package com.LickingHeights;
+
+public class BMI {
+    public static void main(String[] args) {
+        //Declare
+        String userName;
+        String greeting;
+        int lbs;
+        int inches;
+        int m;
+        int kg;
+
+    }
+}
